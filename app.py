2ea5a4810b220973c4b6449fd3d04f98a6051f2c
ismedusoftsol <139052421+ismedusoftsol@users.noreply.github.com>
2023-07-09 18:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 import streamlit as st
 from functions import *
 
-openai.api_key = "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"
+# openai.api_key = "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"
+
+openai.api_key = os.getenv("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")
 
 
 # Replace "OPENAI_API_KEY" with the name of your environment variable
