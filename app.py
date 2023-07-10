e1c1eb8efcceb193e7568d519dc9ebff74dace83
ismedusoftsol <139052421+ismedusoftsol@users.noreply.github.com>
2023-07-10 11:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from functions import *
 
 # openai.api_key = "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"
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("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")
 
 
 # Replace "OPENAI_API_KEY" with the name of your environment variable
